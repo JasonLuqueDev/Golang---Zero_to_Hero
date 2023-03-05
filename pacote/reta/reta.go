736431f1c7b18ff38df3f3d9e18979c50b7954c0
JasonLuqueDev <jason.luque.dev@gmail.com>
2023-03-05 15:11:52 -0300

    test

---
+++ pacote/reta/reta.go
@@ -0,0 +1,13 @@
+package main
+
+// Inicializando com letra MAIUSCULA o pacote é PÚBLICO
+// (Visivel DENTRO E FORA DO PACOTE)
+
+//
+
+// Inicializando com letra '' minúscula ''
+// (Visivel APENAS dentro do pacote)
+
+func main() {
+
+}
